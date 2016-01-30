12cf8abb6c4199ed6fa8d836210a15ffdefbf99a
Sebberrou <seb.berrou@laposte.net>
2016-01-30 22:23:01 +0100

    Starting animating !

--- GGJ16/Assets/Seb/entities/Players/MovePlayer.cs
+++ GGJ16/Assets/Seb/entities/Players/MovePlayer.cs
@@ -4,19 +4,20 @@ using System.Collections;
 public class MovePlayer : Entities {
 	public Rigidbody2D rb2D;
 	//public static bool developerConsoleVisible;
-	public Animation anim;
+	public Animator anim;
 	//Choisir le numéro du joueur
 	public string joystick;
 	public float speed;
-
-	//orientation du personnage
-	public string face;
+	public string playerType;
 
 
 	void Start() {
 		rb2D = GetComponent<Rigidbody2D>();
 		rb2D.freezeRotation = true;
-		anim = GetComponent<Animation>();
+		anim = GetComponent<Animator>();
+		anim.SetBool ("Virgin", false);
+		//Défini la direction 0:top, 1:right, 2: bottom, 3:left
+		anim.SetInteger ("Direction",0);
 	}
 	void Update()
 	{
@@ -26,43 +27,42 @@ public class MovePlayer : Entities {
 
 		//Debug.Log("x : "+x+", y : "+y);
 		if (x == 0 && y == 0) {
-			if (face=="down") {
+			anim.SetBool("Move",false);
+			/*if (face=="down") {
 				//debug = "idleDown_"+state;
-				//animation.Play("idleDown_"+state)
+
 			}else if(face=="up"){
 				//debug = "idleUp_"+state;
-				//animation.Play("idleUp_"+state) 
+				animation.Play("idleUp_"+state); 
 			}else if(face=="left"){
 				//debug = "idleLeft_"+state;
 				//animation.Play("idleLeft_"+state) 
 			}else if(face=="right"){
 				//debug = "idleRight_"+state;
 				//animation.Play("idleRight_"+state) 
-			}
+			}*/
 			
 		}
 		else if(y>0 && x==0){
 			//animation.Play("walkUp_"+state)
 			//debug="walkUp_"+state;
-			face = "up";
+			anim.SetInteger ("Direction",0);
 		}
 		else if(y<0 && x==0){
 			//animation.Play("walkDown_"+state)
 			//debug="walkDown_"+state;
-			face = "down";
+			anim.SetInteger ("Direction",2);
 
 		}
 		if(x>0 && y==0){
 			//animation.Play("walk_"+state)
 			//debug="walk_"+state;
-			face = "right";
-			transform.eulerAngles = new Vector3(0, 0, 0);//Normal
+			anim.SetInteger ("Direction",1);
 		}	
 		else if(x<0 && y==0){
 			//animation.Play("walk_"+state)
 			//debug="walk_"+state;
-			face = "left";
-			transform.eulerAngles = new Vector3(0, 180, 0); //Flipped
+			anim.SetInteger ("Direction",3);
 		}
 		//Debug.Log (debug);
 	}
