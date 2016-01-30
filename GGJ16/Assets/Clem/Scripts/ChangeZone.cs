4c5c22f5a326157c35eaedc4acabb6d71e3cdf16
Weavel35 <clement.chardevel@gmail.com>
2016-01-30 18:55:34 +0100

    TILED FUCK YEAH

--- GGJ16/Assets/Clem/Scripts/ChangeZone.cs
+++ GGJ16/Assets/Clem/Scripts/ChangeZone.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class ChangeZone : MonoBehaviour {
 	
-	 
-
+	public ChangeZone Destination;
+	public Collider2D thisCollider { get; set; }
 	// Use this for initialization
 	void Start () {
-		
+		if(Destination == null)
+			Debug.LogError("pas de destionation", this.gameObject);
+		thisCollider = this.GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
@@ -16,8 +18,25 @@ public class ChangeZone : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
+		
 		if(coll.tag.Equals("Player")){
 			Debug.Log("player!");
 		}
+		player collisionPlayer = coll.GetComponent<player>();
+		if(collisionPlayer!=null)
+			if(!collisionPlayer.isTeleported ) {
+				collisionPlayer.isTeleported=true;
+				collisionPlayer.tpOrigin = thisCollider;
+				
+				collisionPlayer.transform.position=Destination.transform.position;
+			}
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+		player collisionPlayer = coll.GetComponent<player>();
+		if(collisionPlayer!=null)
+			if(collisionPlayer.isTeleported && !collisionPlayer.tpOrigin.Equals(thisCollider)) {
+				collisionPlayer.isTeleported=false;
+			}
 	}
 }
---
+++ GGJ16/Assets/Clem/Scripts/player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public bool isTeleported { get; set; }
+	public Collider2D tpOrigin { get; set; }
+
+	// Use this for initialization
+	void Start () {
+		isTeleported = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
