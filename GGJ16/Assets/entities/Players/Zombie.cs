7429acfcbf5c57559385914292878e5e2a08ada4
Sebberrou <seb.berrou@laposte.net>
2016-01-30 14:01:22 +0100

    action perso

---
+++ GGJ16/Assets/entities/Players/Zombie.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : Entities {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
