9a2ed20d71f540cc40ac3c6c5cbd46c0e29c1b30
Yetarse <joaquim.lancesseur@orange.fr>
2016-01-31 03:36:08 +0100

    atk

---
+++ GGJ16/Assets/Attack+SpecialMove/Assets/entities/Players/ActionPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionPlayer : Entities {
+
+        
+
+
+
+
+
+	//// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+	//	if (Input.GetAxisRaw("Att_" + joystick)) {
+	//		debug = "Att_" + state + joystick;
+	//	}
+	//	if (Input.GetAxisRaw("Act_" + joystick)) {
+	//		debug = "Act_" + state  + joystick;
+	//	}
+	//}
+}
---
+++ GGJ16/Assets/Attack+SpecialMove/Assets/entities/Players/Succubus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Succubus : Entities {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw ("Spe_" + joystick) && state == "default") {
+			debug = "Spe_";
+		}
+
+
+	}
+
+    
+}
