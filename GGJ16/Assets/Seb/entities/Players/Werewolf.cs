649db6d15a178ebb4b1156b67e39ef6b04f577d7
Sebberrou <seb.berrou@laposte.net>
2016-01-31 09:38:50 +0100

    all character, strangly looking like moon-moon-

--- GGJ16/Assets/Seb/entities/Players/Werewolf.cs
+++ GGJ16/Assets/Seb/entities/Players/Werewolf.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Werewolf : Entities {
-	public MonoBehaviour playerAttacks;
+public class Werewolf : MonoBehaviour {
+	public Animator anim;
+	//public MonoBehaviour playerAttacks;
 	// Use this for initialization
 	void Start () {
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown ("Spe_2")) {
+			anim.SetBool("Spe",true);
+		}
 	}
+
 }
