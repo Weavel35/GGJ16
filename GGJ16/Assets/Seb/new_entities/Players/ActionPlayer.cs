36d9122abc25deb09dc092a136a85da5cad108aa
Yetarse <joaquim.lancesseur@orange.fr>
2016-01-31 01:53:59 +0100

    nouveau script

---
+++ GGJ16/Assets/Seb/new_entities/Players/ActionPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionPlayer : MonoBehaviour {
+	
+	public int attack = 1;
+	
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
